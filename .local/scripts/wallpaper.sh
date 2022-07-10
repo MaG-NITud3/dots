c61cba6f4941a222984dcab52fd98d13463beef4
MaG-NITud3 <theeparthasarathi@gmail.com>
2022-07-10 13:41:05 +0530

    update

--- .local/scripts/wallpaper.sh
+++ .local/scripts/wallpaper.sh
@@ -4,4 +4,4 @@ nitrogen --set-scaled "$1" --save > /dev/null 2>&1 &
 cp ~/.cache/wal/colors.Xresources ~/.Xresources > /dev/null 2>&1 &
 cat ~/.cache/wal/kitty.conf >> .config/kitty/kitty.conf 2>&1 &
 cp ~/cache/wal/nord.rasi ~/.config/rofi/themes/nord.rasi 2>&1 &
-bspc wm -r
+bspc wm -r &
