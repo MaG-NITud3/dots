724cd9b50af30cecb0b6a071bad69cc99e6e83a4
MaG-NITud3 <theeparthasarathi@gmail.com>
2022-07-20 19:23:37 +0530

    finalise dunst pywal with critical icon too (hopefullt never have to touch dunst again)

--- .cache/wal/colors-tty.sh
+++ .cache/wal/colors-tty.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 [ "${TERM:-none}" = "linux" ] && \
-    printf '%b' '\e]P0120c13
-                 \e]P1A55165
-                 \e]P2D46956
-                 \e]P3EF9F5C
-                 \e]P4F2AF5A
-                 \e]P58E5487
-                 \e]P6F0B38B
-                 \e]P7f2e2c7
-                 \e]P8a99e8b
-                 \e]P9A55165
-                 \e]PAD46956
-                 \e]PBEF9F5C
-                 \e]PCF2AF5A
-                 \e]PD8E5487
-                 \e]PEF0B38B
-                 \e]PFf2e2c7
+    printf '%b' '\e]P0050614
+                 \e]P1525365
+                 \e]P2988762
+                 \e]P333608E
+                 \e]P450709E
+                 \e]P55E8AB1
+                 \e]P667A0C6
+                 \e]P7c8cecc
+                 \e]P88c908e
+                 \e]P9525365
+                 \e]PA988762
+                 \e]PB33608E
+                 \e]PC50709E
+                 \e]PD5E8AB1
+                 \e]PE67A0C6
+                 \e]PFc8cecc
                  \ec'
